f275703d70a492479160ec259ee6fe492fe38ff1
Saad Khurshid Qurashi <147279806+Isaadqurashi@users.noreply.github.com>
2024-04-21 13:17:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import customtkinter as ctk
-ctk.set_appearance_mode("System")
-
-
-ctk.set_default_color_theme("green")
-
-
 
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.geometry("500x400")
+        ctk.set_default_color_theme("green")
+        self.create_login_widgets()
+
+    def create_login_widgets(self):
+        ctk.CTkEntry(master=self, placeholder_text="Enter your email").pack(pady=10, padx=20)
+        ctk.CTkEntry(master=self, placeholder_text="Enter your password").pack(pady=10, padx=20)
+        ctk.CTkEntry(master=self, placeholder_text="Confirm your password").pack(pady=10, padx=20)
+        ctk.CTkButton(master=self, text="Login", command=self.login_clicked).pack(pady=50, padx=20)
+
+    def login_clicked(self):
+        self.destroy()
+        options_window = OptionsWindow()
+        options_window.mainloop()
+
+class OptionsWindow(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.title("BlindSight")
         self.geometry("500x500")
-        ctk.CTkComboBox(master=self, values=["Caretaker 1", "Caretaker 2", "Caretaker 3"]).pack(
-            pady=20, padx=20
-        )
-        ctk.CTkEntry(master=self, placeholder_text="write your task").pack(
-            pady=20, padx=20
-        )
-        ctk.CTkRadioButton(master=self, text="Confirm the option").pack(pady=20, padx=20)
-        ctk.CTkButton(master=self, text="send").pack(pady=20, padx=20)
-        ctk.CTkSwitch(master=self, text="get notification").pack(pady=20, padx=20)
+        ctk.set_appearance_mode("System")
+        ctk.set_default_color_theme("green")
+        self.create_options_widgets()
 
+    def create_options_widgets(self):
+        ctk.CTkComboBox(master=self, values=["Caretaker 1", "Caretaker 2", "Caretaker 3"]).pack(pady=20, padx=20)
+        ctk.CTkEntry(master=self, placeholder_text="Write your task").pack(pady=10, padx=20)
+        ctk.CTkRadioButton(master=self, text="Confirm the option").pack(pady=10, padx=20)
+        ctk.CTkButton(master=self, text="Send").pack(pady=10, padx=20)
+        ctk.CTkSwitch(master=self, text="Get notification").pack(pady=10, padx=20)
 
 if __name__ == "__main__":
     app = App()
